64ac522f5099fed41eb26f1c2230cd3e0483c278
Dmitry Cherenkov <boogiedimik@gmail.com>
2022-07-27 22:47:56 +0300

    project added

---
+++ f0almark/Shared/f0almarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  f0almarkApp.swift
+//  Shared
+//
+//  Created by Dmitry Cherenkov on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct f0almarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
